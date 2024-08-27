5b9ecab9a764db2091370244fa2b0c809fcd3bf1
Gabriel Samoylov <gsamo122@gmail.com>
2024-08-27 22:31:16 +0300

    complete: Tape animation

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -19,8 +19,8 @@ export const TapeSection = () => {
     <section className="py-16 lg:py-24 overflow-x-clip">
       <div className="bg-gradient-to-r from-emerald-300 to-sky-400 -rotate-3 -mx-1">
         <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
-          <div className="flex flex-none gap-4 py-3">
-            {words.map((word, index) => (
+          <div className="flex flex-none gap-4 pr-4 py-3 animate-move-left [animation-duration:30s]">
+            {[...words, ...words].map((word, index) => (
               <div key={index} className="inline-flex gap-4 items-center">
                 <span className="text-gray-900 uppercase font-extrabold text-sm">
                   {word}
