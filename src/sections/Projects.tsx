3247d52e14fd2f353ae2afa67a574946cd018b08
Gabriel Samoylov <gsamo122@gmail.com>
2024-08-27 09:08:36 +0300

    add: Testimonials UI

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -6,6 +6,8 @@ import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
 import CheckIcon from "@/assets/icons/check-circle.svg";
 
 import Image from "next/image";
+import { SectionHeader } from "@/components/SectionHeader";
+import { Card } from "@/components/Card";
 
 const portfolioProjects = [
   {
@@ -50,67 +52,56 @@ export const ProjectsSection = () => {
   return (
     <section className="pb-16 lg:py-24">
       <div className="container">
-        <div className="flex justify-center">
-          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent text-center">
-            Real-wolrd Results
-          </p>
-        </div>
-        <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">
-          Featured Projects
-        </h2>
-        <p className="text-center text-white/60 mt-4 md:text-lg lg:text-xl max-w-md mx-auto">
-          See how i tramsformed concepts into engaging digitial experiences.
-        </p>
+        <SectionHeader
+          eyebrow="Real-wolrd Results"
+          title="Featured Projects"
+          description="See how i tramsformed concepts into engaging digitial experiences."
+        />
         <div className="mt-10 md:mt-20 flex flex-col gap-20">
-          {portfolioProjects.map((project) => {
-            return (
-              <div
-                key={project.title}
-                className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 pt-8 px-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20 after:pointer-events-none"
-              >
-                <div
-                  className="absolute inset-0 -z-10 opacity-5"
-                  style={{
-                    backgroundImage: `url(${grainImage.src})`
-                  }}
-                ></div>
-                <div className="lg:grid lg:grid-cols-2 lg:gap-16">
-                  <div className="lg:pb-16">
-                    <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
-                      <span className="">{project.company}</span>
-                      <span className="">&bull;</span>
-                      <span className="">{project.year}</span>
-                    </div>
-                    <h3 className="font-serif text-2xl mt-2 md:text-4xl md:mt-5">
-                      {project.title}
-                    </h3>
-                    <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
-                    <ul className="flex flex-col gap-4 mt-4 md:mt-5">
-                      {project.results.map((result) => (
-                        <li className="flex gap-2 text-sm md:text-base text-white/50">
-                          <CheckIcon className="size-5 md:size-6" />
-                          <span className="">{result.title}</span>
-                        </li>
-                      ))}
-                    </ul>
-                    <a href={project.link} target="_blank" className="">
-                      <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
-                        <span className="">Visit Live Site</span>
-                        <ArrowUpRightIcon className="size-4" />
-                      </button>
-                    </a>
+          {portfolioProjects.map((project, index) => (
+            <Card
+              key={index}
+              className="pt-8 px-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20"
+            >
+              <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+                <div className="lg:pb-16">
+                  <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
+                    <span className="">{project.company}</span>
+                    <span className="">&bull;</span>
+                    <span className="">{project.year}</span>
                   </div>
-                  <div className="relative">
-                    <Image
-                      src={project.image}
-                      alt={project.title}
-                      className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"
-                    />
-                  </div>
+                  <h3 className="font-serif text-2xl mt-2 md:text-4xl md:mt-5">
+                    {project.title}
+                  </h3>
+                  <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+                  <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+                    {project.results.map((result, index) => (
+                      <li
+                        key={index}
+                        className="flex gap-2 text-sm md:text-base text-white/50"
+                      >
+                        <CheckIcon className="size-5 md:size-6" />
+                        <span className="">{result.title}</span>
+                      </li>
+                    ))}
+                  </ul>
+                  <a href={project.link} target="_blank" className="">
+                    <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                      <span className="">Visit Live Site</span>
+                      <ArrowUpRightIcon className="size-4" />
+                    </button>
+                  </a>
+                </div>
+                <div className="relative">
+                  <Image
+                    src={project.image}
+                    alt={project.title}
+                    className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"
+                  />
                 </div>
               </div>
-            );
-          })}
+            </Card>
+          ))}
         </div>
       </div>
     </section>
